a228198a70cb3f801c527a986f781e4a756a304d
hols <adholstein@gmail.com>
2019-11-22 18:06:59 +0000

    improve error handling updated readme

--- src/controllers/sql/category.controller.js
+++ src/controllers/sql/category.controller.js
@@ -49,10 +49,10 @@ const crudCat = {
     delete(req, res, next) {
         try {
             catModel.delete(req.params, function (data) {
-                res.json({success: true, data: data})
+                res.json({ success: true, data: data })
             }, next)
         } catch (error) {
-            next(error) && res.json({success: false, err: error})
+            next(error)
         }
     }
 }
