c5d19133a0520876bc4aa440810fe260af82e449
hols <adholstein@gmail.com>
2019-11-23 18:38:51 +0000

    user CRUD and some updates

--- src/models/sql/category.model.js
+++ src/models/sql/category.model.js
@@ -65,13 +65,6 @@ const crudCategory = {
             } /** This is the proper way to handle errors */
             else result(rows)
         })
-    },
-    delete({ id }, result, next) {
-            let query = `delete from category where idCategory = ${id}`
-            sql.query(query, (error, rows, fields) => {
-                if (error) next(error) /** This is the proper way to handle errors */
-                else result(rows)
-            })
     }
 }
 
