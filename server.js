176d8b55413da0f9bf9631f1bf68f0d150e4711c
hols <adholstein@gmail.com>
2019-12-12 12:08:38 +0000

    correct auth path

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const achievementsRoute = require(generalRoutesPath + "achievement.route");
 const sensorRoute = require(generalRoutesPath + "sensor.route");
 const userRoute = require(generalRoutesPath + "user.route");
 const packageRoute = require(generalRoutesPath + "package.route")
+const authRoute = require(generalRoutesPath+"auth.route")
+
 /** Our middlewares */
 const testMiddleware = require(ourMiddlewarePath + "test/test.mid.js");
 const sanitizerMiddleware = require(ourMiddlewarePath + "sanitizer.middleware")
