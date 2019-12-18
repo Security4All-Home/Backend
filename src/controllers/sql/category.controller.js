3bd08aa131ce70692ad134b3256e188527c4d96f
JoaoCampos99 <j_henrique_campos@hotmail.com>
2019-12-18 12:10:13 +0000

    merge problems solved

--- src/controllers/sql/category.controller.js
+++ src/controllers/sql/category.controller.js
@@ -45,24 +45,6 @@ const crudCat = {
         } catch (error) {
             next(error)
         }
-    },
-    delete(req, res, next) {
-        try {
-            catModel.delete(req.params, function (data) {
-                res.json({ success: true, data: data })
-            }, next)
-        } catch (error) {
-            next(error)
-        }
-    },
-    delete(req, res, next) {
-        try {
-            catModel.delete(req.params, function (data) {
-                res.json({ success: true, data: data })
-            }, next)
-        } catch (error) {
-            next(error)
-        }
     }
 }
 
