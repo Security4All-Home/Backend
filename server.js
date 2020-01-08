863fed8baf34db117f7283b39b4219dda46bdbf7
JoaoCampos99 <j_henrique_campos@hotmail.com>
2020-01-08 21:57:17 +0000

    sensor_space

--- server.js
+++ server.js
@@ -14,19 +14,17 @@ const errorHandler = require(experimentalRoutesPath + "errorhandling.route")
 const testRoute = require(experimentalRoutesPath + "tests.route");
 const getMacAdressRoute = require(experimentalRoutesPath + "getMacAdress");
 
-
 /** "Real" Routes */
 const categoryRoute = require(generalRoutesPath + "category.route");
 const achievementsRoute = require(generalRoutesPath + "achievement.route");
 const sensorRoute = require(generalRoutesPath + "sensor.route");
 const userRoute = require(generalRoutesPath + "user.route");
-const packageRoute = require(generalRoutesPath + "package.route")
-const authRoute = require(generalRoutesPath+"auth.route")
+const packageRoute = require(generalRoutesPath + "package.route");
+const authRoute = require(generalRoutesPath + "auth.route");
 
 /** Our middlewares */
 const testMiddleware = require(ourMiddlewarePath + "test/test.mid.js");
-const sanitizerMiddleware = require(ourMiddlewarePath + "sanitizer.middleware")
-
+const sanitizerMiddleware = require(ourMiddlewarePath + "sanitizer.middleware");
 
 /** Middlewares */
 // server.use(cors()) // Não pode ficar assim depois
@@ -34,7 +32,7 @@ server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 server.use(cookieParser());
 // server.use(testMiddleware.visualizeHeaders);
-server.use(sanitizerMiddleware)
+//server.use(sanitizerMiddleware)
 
 server.use("/macadress", getMacAdressRoute);
 server.use("/test", testRoute);
@@ -47,7 +45,6 @@ server.use("/achievement", achievementsRoute);
 server.use("/sensors", sensorRoute);
 server.use("/user", userRoute);
 server.use("/packages", packageRoute);
-
 
 server.get("/home", (req, res) => {
   res.send(`
