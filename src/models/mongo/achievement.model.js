a86ffac2081e0354498f5e4f8995c8cfee620fff
unknown <rodrigodlq@hotmail.com>
2020-01-15 13:40:18 +0000

    ach update

--- src/models/mongo/achievement.model.js
+++ src/models/mongo/achievement.model.js
@@ -24,6 +24,16 @@ const achievementSchema = new Schema({
         required: true,
         default: null
     },
+    image: {
+        type: String,
+        unique: false,
+        required: true,
+        default: null
+    },
+    imageDefault: {
+        type: String,
+        default: "https://pbs.twimg.com/media/ENwUVtvWkAA3k5x?format=png&name=360x360"
+    }
 })
 
 const achievementModel = model("achievement", achievementSchema);
