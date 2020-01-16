58206bfe467d8df231e2dbe22a259d562abf854a
JoaoCampos99 <j_henrique_campos@hotmail.com>
2020-01-16 18:59:47 +0000

    sql reconnect bug fix

--- src/sqlconnection.js
+++ src/sqlconnection.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 /**
  * Handle lost connection by mysql
@@ -6,48 +6,47 @@ const mysql = require('mysql');
  */
 
 const dbConfig = {
-    host: process.env.serverSql,
-    user: process.env.rootUserSql,
-    password: process.env.rootPassSql,
-    database: process.env.databaseSql,
-    port: process.env.portSql,
-    multipleStatements: true,
-    // connectTimeout: 1860000
-}
-
+  host: process.env.serverSql,
+  user: process.env.rootUserSql,
+  password: process.env.rootPassSql,
+  database: process.env.databaseSql,
+  port: process.env.portSql,
+  multipleStatements: true
+  // connectTimeout: 1860000
+};
 
 /**
  * recover after fatal, ver depois (https://stackoverflow.com/questions/33652697/node-js-process-cannot-recover-after-mysql-turned-off-then-turn-on)
  */
 let connection = null;
 function connectToDatabase() {
-    connection = mysql.createConnection(dbConfig);
+  connection = mysql.createConnection(dbConfig);
 
-    connection.connect((err) => {
-        if (err) {
-            console.log(err, "Error ocurred in mysql")
-            return;
-        }
-        // connection.query('SET PERSIST interactive_timeout=1860000;')
-        console.time('tempo de atividade DB');
-        console.log('SQL Database connected with the id ' + connection.threadId)
-    })
+  connection.connect(err => {
+    if (err) {
+      console.log(err, "Error ocurred in mysql");
+      setTimeout(connectToDatabase, 2000); // We introduce a delay before attempting to reconnect,
+    }
+    // connection.query('SET PERSIST interactive_timeout=1860000;')
+    console.time("tempo de atividade DB");
+    console.log("SQL Database connected with the id " + connection.threadId);
+  });
 
-    connection.on('end', (parameter) => {
-        // Não é isto que apanha o erro quando a conexão fecha por demasiado tempo de inatividade
-        console.log(parameter)
-    })
+  connection.on("end", parameter => {
+    // Não é isto que apanha o erro quando a conexão fecha por demasiado tempo de inatividade
+    console.log(parameter);
+  });
 
-    connection.on('error', (error) => {
-        console.timeEnd('tempo de atividade DB');
-        // console.log(error, "Tentar apanhar erro de inatividade");
-        console.log(error.code, "Connection Error Code!");
-        if (error.code == "PROTOCOL_CONNECTION_LOST") {
-            connection.end();
-            console.log("VOLTAR A FAZER A CONEXÃO")
-            connectToDatabase();
-        }
-    })
+  connection.on("error", error => {
+    console.timeEnd("tempo de atividade DB");
+    // console.log(error, "Tentar apanhar erro de inatividade");
+    console.log(error.code, "Connection Error Code!");
+    if (error.code === "PROTOCOL_CONNECTION_LOST") {
+      connectToDatabase();
+    } else {
+      throw error;
+    }
+  });
 }
 
 connectToDatabase();
