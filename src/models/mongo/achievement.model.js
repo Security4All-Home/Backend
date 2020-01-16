f2a891dff9dc625e12f96a0d9f7c0dee99217054
JoaoCampos99 <j_henrique_campos@hotmail.com>
2020-01-16 19:49:55 +0000

    minor change

--- src/models/mongo/achievement.model.js
+++ src/models/mongo/achievement.model.js
@@ -2,31 +2,28 @@ const Schema = require("mongoose").Schema;
 const model = require("mongoose").model;
 
 const achievementSchema = new Schema({
-    // id: {
-    //     type: Number,
-    //     unique: true,
-    // },
-    description: {
-        type: String,
-        unique: false,
-        required: true,
-        default: null
-    },
-    goal: {
-        type: String,
-        unique: false,
-        required: true,
-        default: null
-    },  
-    imageType: {
-        type: { type: String, default: null },
-        image: { type: String, default: null}
-    },
-    imageDefault: {
-        type: String,
-        default: "https://pbs.twimg.com/media/ENwUVtvWkAA3k5x?format=png&name=360x360"
-    }
-})
+  description: {
+    type: String,
+    unique: false,
+    required: true,
+    default: null
+  },
+  goal: {
+    type: String,
+    unique: false,
+    required: true,
+    default: null
+  },
+  imageType: {
+    type: { type: String, default: null },
+    image: { type: String, default: null }
+  },
+  imageDefault: {
+    type: String,
+    default:
+      "https://pbs.twimg.com/media/ENwUVtvWkAA3k5x?format=png&name=360x360"
+  }
+});
 
 const achievementModel = model("achievement", achievementSchema);
 
