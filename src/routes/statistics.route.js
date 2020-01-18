b0df63b8a9a74b3d3d2fd23d0bef53b529595e13
JoaoCampos99 <j_henrique_campos@hotmail.com>
2020-01-18 14:42:37 +0000

    documenting statistics

--- src/routes/statistics.route.js
+++ src/routes/statistics.route.js
@@ -1,6 +1,113 @@
 const router = require("express").Router();
 const statisticsController = require("../controllers/sql/statistics.controller");
-
+/**
+ * @swagger
+ * paths:
+ *  /statistics/numUsers:
+ *      get:
+ *        tags:
+ *          - statistics
+ *        summary: Vai Buscar o número de utilizadores registados na base de dados
+ *        operationId: getNumUsers
+ *        produces:
+ *          - aplication/json
+ *        responses:
+ *          200:
+ *            description: Operação bem sucedida
+ *          400:
+ *            description: Erro ao realizar esta operação
+ *  /statistics/numSensorsSold:
+ *      get:
+ *        tags:
+ *          - statistics
+ *        summary: Vai Buscar a quantidade de sensores vendidos
+ *        operationId: getNumSensorsSold
+ *        produces:
+ *          - aplication/json
+ *        responses:
+ *          200:
+ *            description: Operação bem sucedida
+ *          400:
+ *            description: Erro ao realizar esta operação
+ *  /statistics/installation:
+ *      get:
+ *        tags:
+ *          - statistics
+ *        summary: Vai Buscar o número de pedidos de instalação na base de dados
+ *        operationId: getInstallation
+ *        produces:
+ *          - aplication/json
+ *        responses:
+ *          200:
+ *            description: Operação bem sucedida
+ *          400:
+ *            description: Erro ao realizar esta operação
+ *  /statistics/ordersToPay:
+ *      get:
+ *        tags:
+ *          - statistics
+ *        summary: Vai Buscar o número de encomendas por pagar na base de dados
+ *        operationId: getNumOrderToPay
+ *        produces:
+ *          - aplication/json
+ *        responses:
+ *          200:
+ *            description: Operação bem sucedida
+ *          400:
+ *            description: Erro ao realizar esta operação
+ *  /statistics/numHouses:
+ *      get:
+ *        tags:
+ *          - statistics
+ *        summary: Vai Buscar o número de casas existentes na base de dados
+ *        operationId: getNumHouses
+ *        produces:
+ *          - aplication/json
+ *        responses:
+ *          200:
+ *            description: Operação bem sucedida
+ *          400:
+ *            description: Erro ao realizar esta operação
+ *  /statistics/avgSensor:
+ *      get:
+ *        tags:
+ *          - statistics
+ *        summary: Vai Buscar a média de sensores por casa
+ *        operationId: getAvgSensor
+ *        produces:
+ *          - aplication/json
+ *        responses:
+ *          200:
+ *            description: Operação bem sucedida
+ *          400:
+ *            description: Erro ao realizar esta operação
+ *  /statistics/usersToValidate:
+ *      get:
+ *        tags:
+ *          - statistics
+ *        summary: Vai Buscar o número de utilizadores registados que ainda não validaram a sua conta na base de dados
+ *        operationId: getNumUsersToValidate
+ *        produces:
+ *          - aplication/json
+ *        responses:
+ *          200:
+ *            description: Operação bem sucedida
+ *          400:
+ *            description: Erro ao realizar esta operação
+ *  /statistics/numAchievements:
+ *      get:
+ *        tags:
+ *          - statistics
+ *        summary: Vai Buscar o número de conquistas registadas na base de dados
+ *        operationId: getNumAchievements
+ *        produces:
+ *          - aplication/json
+ *        responses:
+ *          200:
+ *            description: Operação bem sucedida
+ *          400:
+ *            description: Erro ao realizar esta operação
+ */
 router.get("/numUsers", (req, res, next) => {
   statisticsController.numberOfUsers(req, res, next);
 });
